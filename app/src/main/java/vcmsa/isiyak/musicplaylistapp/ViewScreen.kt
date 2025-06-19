6aa22f5d96dec7d439dbe4110b6708d85b450896
Istux <patelistiyak6@gmail.com>
2025-06-19 13:44:56 +0200

    initial commit

--- app/src/main/java/vcmsa/isiyak/musicplaylistapp/ViewScreen.kt
+++ app/src/main/java/vcmsa/isiyak/musicplaylistapp/ViewScreen.kt
@@ -1,11 +1,14 @@
 package vcmsa.isiyak.musicplaylistapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
 
 class ViewScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,11 @@ class ViewScreen : AppCompatActivity() {
             insets
         }
         //code starts here
+        val btnterminate = findViewById<Button>(R.id.btnTerminate)
+        val btnBackToMainPage = findViewById<Button>(R.id.btnBackToMainPage)
+
         //receives the data from the activity main.
+
         val songListText = findViewById<TextView>(R.id.TvListOfSongs)
         val averageRatingText = findViewById<TextView>(R.id.TvAverageRating)
         //retrieves the data from the intent
@@ -40,5 +47,19 @@ class ViewScreen : AppCompatActivity() {
         } else 0.0
         //calculates the average rating
         averageRatingText.text = "Average Rating: %.2f".format(average)
+
+        btnterminate.setOnClickListener {
+            finishAffinity()
+            exitProcess(0)
+        }
+        btnBackToMainPage.setOnClickListener {
+            finish()
+
+            btnBackToMainPage.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
     }
 }
